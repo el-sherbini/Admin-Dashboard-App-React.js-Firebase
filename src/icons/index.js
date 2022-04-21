6abbe23b7c84347a7898fad4a0d481bfafb95582
Mohamed Emad <mohamed.elsherbiny215@gmail.com>
2022-04-21 17:03:10 +0200

    add widget

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,3 +1,4 @@
+// Sidebar icons
 export { default as DashboardIcon } from "@mui/icons-material/Dashboard";
 export { default as PersonOutlineIcon } from "@mui/icons-material/PersonOutline";
 export { default as LocalShippingIcon } from "@mui/icons-material/LocalShipping";
@@ -10,9 +11,18 @@ export { default as NotificationsNoneIcon } from "@mui/icons-material/Notificati
 export { default as SettingsSystemDaydreamOutlinedIcon } from "@mui/icons-material/SettingsSystemDaydreamOutlined";
 export { default as PsychologyOutlinedIcon } from "@mui/icons-material/PsychologyOutlined";
 export { default as AccountCircleOutlinedIcon } from "@mui/icons-material/AccountCircleOutlined";
+
+// Navbar icons
 export { default as SearchOutlinedIcon } from "@mui/icons-material/SearchOutlined";
 export { default as DarkModeOutlinedIcon } from "@mui/icons-material/DarkModeOutlined";
 export { default as FullscreenExitOutlinedIcon } from "@mui/icons-material/FullscreenExitOutlined";
 export { default as NotificationsNoneOutlinedIcon } from "@mui/icons-material/NotificationsNoneOutlined";
 export { default as ChatBubbleOutlineOutlinedIcon } from "@mui/icons-material/ChatBubbleOutlineOutlined";
 export { default as ListOutlinedIcon } from "@mui/icons-material/ListOutlined";
+
+// Widget icons
+export { default as KeyboardArrowUpIcon } from "@mui/icons-material/KeyboardArrowUp";
+export { default as PersonOutlinedIcon } from "@mui/icons-material/PersonOutlined";
+export { default as AccountBalanceWalletOutlinedIcon } from "@mui/icons-material/AccountBalanceWalletOutlined";
+export { default as ShoppingCartOutlinedIcon } from "@mui/icons-material/ShoppingCartOutlined";
+export { default as MonetizationOnOutlinedIcon } from "@mui/icons-material/MonetizationOnOutlined";
